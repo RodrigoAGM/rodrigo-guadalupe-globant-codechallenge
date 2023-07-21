7afda490d7833dd97ead71b2186fa2ac70b371d8
RodrigoAGM <rod-gm@hotmail.com>
2023-07-21 02:08:37 -0500

    chore: add base enums

---
+++ verification-service/src/data/enum.ts
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-shadow */
+
+export enum StatusCodes {
+  SUCCESS = 200,
+  BAD_REQUEST = 400,
+  INTERNAL_ERROR = 500,
+}
+
+export enum HTTPMethods {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  PATCH = 'PATCH',
+  DELETE = 'DELETE',
+}

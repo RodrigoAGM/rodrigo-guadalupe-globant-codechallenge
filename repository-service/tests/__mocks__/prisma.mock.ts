c01460c531c468f37786141add7a7867e4f3c677
RodrigoAGM <rod-gm@hotmail.com>
2023-07-21 13:10:44 -0500

    test: setup repository service tests

---
+++ repository-service/tests/__mocks__/prisma.mock.ts
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+import 'reflect-metadata';
+import { PrismaClient } from '@prisma/client';
+import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended';
+
+import prisma from './prisma.client';
+
+jest.mock('./prisma.client', () => ({
+  __esModule: true,
+  default: mockDeep<PrismaClient>(),
+}));
+
+beforeEach(() => {
+  // eslint-disable-next-line no-use-before-define
+  mockReset(prismaMock);
+});
+
+export const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>;
